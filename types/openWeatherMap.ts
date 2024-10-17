07507377aea60ec20947b3b8b6dadd61eaa538fe
Andrew floatrx <ifloatrx@gmail.com>
2024-10-17 19:28:17 +0300

    add types

---
+++ types/openWeatherMap.ts
@@ -0,0 +1,102 @@
+interface WeatherCondition {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface CurrentWeather {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  weather: WeatherCondition[];
+}
+
+interface MinutelyForecast {
+  dt: number;
+  precipitation: number;
+}
+
+interface HourlyForecast {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: WeatherCondition[];
+  pop: number;
+}
+
+interface DailyTemperature {
+  day: number;
+  min: number;
+  max: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+interface DailyFeelsLike {
+  day: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+interface DailyForecast {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  summary: string;
+  temp: DailyTemperature;
+  feels_like: DailyFeelsLike;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: WeatherCondition[];
+  clouds: number;
+  pop: number;
+  rain?: number;
+  uvi: number;
+}
+
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
+interface WeatherApiResponse {
+  coordinates: Coordinates;
+  loading: boolean;
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  current: CurrentWeather;
+  minutely: MinutelyForecast[];
+  hourly: HourlyForecast[];
+  daily: DailyForecast[];
+}
