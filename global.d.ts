c1c49d534bc6f75e8034321ecefa9dff64aa1836
Andrew floatrx <ifloatrx@gmail.com>
2024-10-17 19:27:49 +0300

    update project config

---
+++ global.d.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// .env
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    API_KEY: string;
+  }
+}
+
+// Global type declarations for React components
+declare global {
+  // Just empty object
+  type EmptyObj = Record<string, unknown>;
+  // Any object
+  type AnyObj = Record<string, any>;
+  // React fn component with children
+  type FC<T = object> = React.FunctionComponent<React.PropsWithChildren<T>>;
+  // React fn component without children
+  type RC<T = object> = React.FunctionComponent<T>;
+  // Get available props from element (e.g. button, input, div, etc.)
+  type ComponentProps<T> = React.ComponentProps<T>;
+  // Next page props
+  type PageProps<Params = EmptyObj, SearchParams = EmptyObj> = {
+    params: Partial<Params>;
+    searchParams: Partial<SearchParams>;
+  };
+}
