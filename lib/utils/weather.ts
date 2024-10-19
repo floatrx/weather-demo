80515409fbff768828d4b48bbbfb77c7f8cbfa34
Andrew floatrx <ifloatrx@gmail.com>
2024-10-19 14:42:24 +0300

    add temperature icon

--- lib/utils/weather.ts
+++ lib/utils/weather.ts
@@ -9,3 +9,13 @@ export const convertTemperature = (kelvin: number, format: 'C' | 'K' | 'F') => {
       return kelvin.toFixed();
   }
 };
+
+/**
+ * Check if the temperature in Kelvin is considered cold (below freezing point of water).
+ * @param kelvin - Temperature in Kelvin
+ * @returns boolean - True if the temperature is below freezing point, otherwise false
+ */
+export const isNegativeTemperature = (kelvin: number): boolean => {
+  const freezingPointInKelvin = 273.15;
+  return kelvin < freezingPointInKelvin;
+};
