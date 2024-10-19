4426d0e8c9a10819c5ec02b7eb0e850dc86a19be
Andrew floatrx <ifloatrx@gmail.com>
2024-10-19 11:46:30 +0300

    fix cards overflow

--- components/ui/SimpleCard.tsx
+++ components/ui/SimpleCard.tsx
@@ -15,7 +15,10 @@ interface CardProps extends ComponentProps<'section'> {
  * @constructor
  */
 export const SimpleCard: FC<CardProps> = ({ children, className, title, ...props }) => (
-  <section className={cn(`card min-w-[170px] max-w-lg flex-1 select-none rounded-lg p-4 shadow-md @container`, className)} {...props}>
+  <section
+    className={cn(`card @container min-w-[170px] max-w-lg flex-1 select-none overflow-hidden rounded-lg p-4 shadow-md`, className)}
+    {...props}
+  >
     {title && (
       <div>
         <h2 className="mb-2 flex items-center gap-2 text-xl font-semibold">{title}</h2>
