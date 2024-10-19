eb89027a3bc0b1ca03abe6c53389fe74c2e641c9
Andrew floatrx <ifloatrx@gmail.com>
2024-10-19 14:54:23 +0300

    update doc string for widget context

--- components/context/WeatherWidgetContext.tsx
+++ components/context/WeatherWidgetContext.tsx
@@ -7,11 +7,21 @@ export interface WeatherWidgetContextType extends UseWeatherReturnType {}
 
 const WeatherWidgetContext = createContext<WeatherWidgetContextType | undefined>(undefined);
 
+/**
+ * Weather widget provider
+ * This provider is used to provide weather data to the nested widget components
+ * All logic encapsulated in useWeatherWidget hook
+ * @param children
+ * @constructor
+ */
 export const WeatherWidgetProvider = ({ children }: { children: ReactNode }) => {
   const value = useWeatherWidget();
   return <WeatherWidgetContext.Provider value={value}>{children}</WeatherWidgetContext.Provider>;
 };
 
+/**
+ * Main hook to use weather context
+ */
 export const useWeatherContext = (): WeatherWidgetContextType => {
   const context = useContext(WeatherWidgetContext);
   if (!context) {
