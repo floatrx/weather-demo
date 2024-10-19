fc788bd41dd3826a8399e11c6c86cf4b8a1af122
Andrew floatrx <ifloatrx@gmail.com>
2024-10-19 19:10:36 +0300

    fix theme toggle component
    - Ensure the theme toggle correctly cycles through 'dark', 'light', and 'system' themes.

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,22 +1,40 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
+import { Moon, Sun, MonitorCog } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/Button';
 import { cn } from '@/lib/utils/cn';
 
+type Theme = 'dark' | 'light' | 'system';
+
+const getNextTheme = (currentTheme: Theme): Theme => {
+  const themeMap: Record<Theme, Theme> = {
+    dark: 'light',
+    light: 'system',
+    system: 'dark',
+  };
+  return themeMap[currentTheme];
+};
+
 export default function ToggleTheme() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme, theme = 'system' } = useTheme();
 
   const toggleTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    if (theme) {
+      setTheme(getNextTheme(theme as Theme));
+    }
   };
 
   return (
     <Button className="size-7" suppressHydrationWarning onClick={toggleTheme} aria-label="Toggle theme" variant="outline" size="icon">
-      <span className={cn('size-4', theme === 'dark' ? 'animate-rotateMoon' : 'animate-rotateSun')}>
-        {theme === 'dark' ? <Moon /> : <Sun />}
+      <span
+        className={cn(
+          'size-4',
+          theme === 'dark' ? 'animate-rotateMoon' : theme === 'light' ? 'animate-rotateSun' : 'animate-rotateSunMoon',
+        )}
+      >
+        {theme === 'dark' ? <Moon /> : theme === 'light' ? <Sun /> : <MonitorCog />}
       </span>
     </Button>
   );
