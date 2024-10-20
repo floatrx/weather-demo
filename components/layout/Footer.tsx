04b4f50c238dc221979cbfb9b6145e84e0a36534
Andrew floatrx <ifloatrx@gmail.com>
2024-10-20 16:55:03 +0300

    fix footer spacing

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 export const Footer: RC = () => (
-  <footer className="container pb-4 text-sm">
+  <footer className="container pb-4 text-sm my-4">
     <p className="max-w-xl">
       &copy; Floatrx 2024 | Weather demo app. Nextjs14 + ShadCN + TailwindCSS (@container queries) + OpenWeatherMap API Source code:
       <Link className="px-2 underline" href="https://github.com/floatrx/weather-demo" rel="noopener" target="_blank">
